d4e9cd40a500aa1c4b6f848fa5c3aef3da7f8b38
Konkinglook <suksansangtup@gmail.com>
2024-07-03 17:16:27 +0700

    animate
    animate

--- js/main.js
+++ js/main.js
@@ -6,3 +6,27 @@ menu.onclick = () =>{
     menu.classList.toggle('move');
     navbar.classList.toggle("open-menu");
 };
+
+// Cloce Menu On Scroll
+window.onscroll = () =>{
+    menu.classList.remove("move");
+    navbar.classList.remove("open-menu");
+}
+
+// ScrollReveal
+const animate = ScrollReveal({
+    origin:'top',
+    distance:'60px',
+    duration:'2500',
+    delay: '400',
+});
+
+animate.reveal(".home-text",{origin: "left" });
+animate.reveal(".home-img",{origin: "bottom" });
+animate.reveal(".heading, . newsletter h2",{origin: "top" });
+animate.reveal(
+    "header, .features-box, .feature-menu-box,.item-box, .t-box, .newsletter form",
+    {
+    interval:100
+    }
+);
