d2f0d7e31bc69096c2b91117ffbddb60ffd4b0a6
Konkinglook <suksansangtup@gmail.com>
2024-07-03 15:56:28 +0700

    Responsive
    Responsive

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 // Menu
 let menu =document.querySelector('.menu-icon');
+let navbar =document.querySelector('.navbar');
 
 menu.onclick = () =>{
     menu.classList.toggle('move');
-}
+    navbar.classList.toggle("open-menu");
+};
